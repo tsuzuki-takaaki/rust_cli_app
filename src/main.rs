b03a0bd57e08c535eeef069e08a2abaf471a23ba
alexanderpierce <takaaki.110706@gmail.com>
2023-04-02 15:18:54 +0900

    add function of searching pattern from given file path

--- src/main.rs
+++ src/main.rs
@@ -7,5 +7,14 @@ use crate::types::cli::Cli;
 
 fn main() {
   let args = Cli::parse();
-  println!("{:?}", args);
+
+  // args.path needs filename extension
+  // correct: cargo run hello src/main.rs
+  // incorrect: cargo run hello src/main
+  let content = std::fs::read_to_string(&args.path).expect("the file is not found");
+  for line in content.lines() {
+    if line.contains(&args.pattern) {
+      println!("{}", line);
+    }
+  }
 }
