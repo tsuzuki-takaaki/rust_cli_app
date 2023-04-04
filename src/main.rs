a709ca90c8950259ea18cbdb947e9e64373136de
alexanderpierce <takaaki.110706@gmail.com>
2023-04-04 23:28:42 +0900

    Enable bufferring for printing

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,8 @@ mod types;
 
 // namespace
 use clap::Parser;
-use std::{io::{BufReader, BufRead}, fs::File};
+use std::{io::{BufReader, BufRead, BufWriter}, fs::File};
+use std::io::Write;
 use crate::types::cli::Cli;
 
 fn main() {
@@ -17,10 +18,12 @@ fn main() {
 }
 
 fn check_pattern(file: BufReader<File>, args: &Cli) {
+  let stdout = std::io::stdout();
+  let mut handle = BufWriter::new(stdout.lock());
   for line in file.lines() {
     // as_ref is so usable like this situation
     if line.as_ref().unwrap().contains(&args.pattern) {
-      println!("{}", line.unwrap());
+      writeln!(handle, "{}", line.unwrap()).unwrap();
     }
   }
 }
